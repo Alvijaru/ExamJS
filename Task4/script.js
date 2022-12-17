d05814b1f01dca3cf5125f9014b7f096bd6ae381
Alvijaru <71391792+Alvijaru@users.noreply.github.com>
2022-12-17 12:23:56 +0200

    Add files via upload

---
+++ Task4/script.js
@@ -0,0 +1,30 @@
+/* ------------------------------ TASK 4 -----------------------------------
+Parašykite JS kodą, kuris vartotojui atėjus į tinklapį kreipsis į cars.json failą ir 
+atvaizduos visus automobilių gamintojus bei pagamintus modelius. 
+Kiekvienas gamintojas turės savo atvaizdavimo "kortelę", kurioje bus 
+nurodomas gamintojas ir jo pagaminti modeliai.
+
+Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), turi 
+būti stilizuota su CSS ir būti responsive;
+-------------------------------------------------------------------------- */
+
+const ENDPOINT = 'cars.json';
+
+fetch('cars.json')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                appendData(data);
+            })
+            .catch(function (err) {
+                console.log('error: ' + err);
+            });
+        function appendData(data) {
+            var mainContainer = document.getElementById("output");
+            for (var i = 0; i < data.length; i++) {
+                var div = document.createElement("div");
+                div.innerHTML = data[i].brand + ' ' + data[i].models;
+                mainContainer.appendChild(div);
+            }
+        }
